50eae831f7ab7d2fc5f6b40db2f3ed8f55008c7b
kevink2022 <kevink2019@gmail.com>
2024-08-30 16:20:31 -0700

    Initial Commit

---
+++ hede/hedeApp.swift
@@ -0,0 +1,17 @@
+//
+//  hedeApp.swift
+//  hede
+//
+//  Created by Kevin Kelly on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct hedeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
